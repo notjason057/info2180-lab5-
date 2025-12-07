8d43136ca40b079bd42eb9ba4b63f7870823047d
Jason Matthews <jasonmatthews@Mac.home.lla.com>
2025-12-07 10:09:02 -0500

    updated world.php

--- world.php
+++ world.php
@@ -17,8 +17,22 @@ if ($country !== '') {
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+<table>
+  <tr>
+    <th>Name</th>
+    <th>Continent</th>
+    <th>Independence Year</th>
+    <th>Head of State</th>
+  </tr>
+
+  <?php foreach ($results as $row): ?>
+    <tr>
+      <td><?= $row['name'] ?></td>
+      <td><?= $row['continent'] ?></td>
+      <td><?= $row['independence_year'] ?></td>
+      <td><?= $row['head_of_state'] ?></td>
+    </tr>
+  <?php endforeach; ?>
+</table>
+
+
